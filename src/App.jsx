211bb6d68cc209d18a49cd76c66b56245272ffa4
roxananavahr <navaroxanahs@gmail.com>
2025-04-08 18:58:07 -0700

    se hicieron cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import SearchBar from "./components/SearchBar"
 import ImageList from "./components/ImageList"
 import searchImage from "./api"
@@ -5,12 +6,18 @@ import searchImage from "./api"
 
 function App() {
   //searchImage('cars')
+const[images, setImages] = useState([])
 
+const handleSubmit = async (term) =>{
+  console.log('Usted esta buscando con : ',term)
+  const result = await searchImage(term)
+  setImages(result)
+}
   return (
     <>
     <h1>Pictures App</h1>
-    <SearchBar/>
-    <ImageList/>
+    <SearchBar onSubmit={handleSubmit}/>
+    <ImageList images={images}/>
     </>
   )
 }
